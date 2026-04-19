7b222a79d2d18df111ed8a81665df4a9d2bf9110
KJWGarden <mike03131313@gmail.com>
2026-04-20 03:49:06 +0900

    유닛 테스트

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import CreateMovieDto from './dto/create-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie | undefined {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
